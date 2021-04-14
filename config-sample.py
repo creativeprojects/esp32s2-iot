4a461272ebe92d98c10e85d9cae6da762f59f32f
Fred <fred@creativeprojects.tech>
2021-04-14 17:23:17 +0100

    add sample config file

---
+++ config-sample.py
@@ -0,0 +1,9 @@
+config = {
+    "name": "esp32s2",
+    "wifi_ssid": "MY WIFI",
+    "wifi_password": "",
+    "mqtt_broker": "mqtt.example.com",
+    "mqtt_port": 1883,
+    "brightness": 0.1,
+    "sleep": 600, # measurement cycle in seconds
+}
